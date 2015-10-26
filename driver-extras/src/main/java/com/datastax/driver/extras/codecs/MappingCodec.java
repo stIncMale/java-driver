8be540feb8ff681d29a956907ceda611b93127b0
Alexandre Dutra <alexandre.dutra@datastax.com>
2015-10-26 17:45:12 +0100

    JAVA-846: Generic codec support for Pojo to Pojo mappings.
    This commit introduces a new general-purpose codec that maps Pojos to another intermediary Java object that is in turn mapped to a CQL type.

--- driver-extras/src/main/java/com/datastax/driver/extras/codecs/MappingCodec.java
+++ driver-extras/src/main/java/com/datastax/driver/extras/codecs/MappingCodec.java
@@ -13,7 +13,7 @@
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  */
-package com.datastax.driver.core;
+package com.datastax.driver.extras.codecs;
 
 import java.nio.ByteBuffer;
 
@@ -24,8 +24,12 @@ import com.datastax.driver.core.TypeCodec;
 import com.datastax.driver.core.exceptions.InvalidTypeException;
 
 /**
- * This class is a copy of MappingCodec declared in the extras module,
- * to avoid circular dependencies between Maven modules.
+ * An abstract TypeCodec that maps a Java Pojo to another Java object
+ * that can in turn be serialized into a CQL type.
+ * This can serve as a base for libraries dealing with Pojo mappings.
+ *
+ * @param <O> The "outer" (user-facing) Java type
+ * @param <I> The "inner" (Cassandra-facing) Java type
  */
 public abstract class MappingCodec<O, I> extends TypeCodec<O> {
 
